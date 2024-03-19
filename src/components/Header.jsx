5a0372a7b50dfff2419f99d5b54a2548c4857996
GuiFraV <guillaume.franguiadakis@gmail.com>
2024-03-19 17:02:33 +0100

    Add fade-in effect to logo2 in Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,27 +5,30 @@ import gsap from "gsap"
 
 const Header = () => {
   const duration = 2; // Durée du fondu en secondes
+  const delay = 3; // Délai en secondes avant de déclencher l'effet
 
   useGSAP(() => {
     gsap.set("#logo2", { opacity: 0, x: 100 }); // Initialise l'opacité du logo 2 à 0 et le décale vers la droite
-    
-    gsap.to("#logo1", {
-      opacity: 0, // Fait disparaître le logo 1
-      duration: duration,
-      ease: "power2.inOut",
-    });
 
-    gsap.to("#logo2", {
-      opacity: 1, // Fait apparaître le logo 2
-      x: 0, // Déplace le logo 2 vers la gauche pour qu'il se superpose au logo 1
-      duration: duration,
-      ease: "power2.inOut",
-    });
+    setTimeout(() => {
+      gsap.to("#logo1", {
+        opacity: 0, // Fait disparaître le logo 1
+        duration: duration,
+        ease: "power2.inOut",
+      });
+
+      gsap.to("#logo2", {
+        opacity: 1, // Fait apparaître le logo 2
+        x: 0, // Déplace le logo 2 vers la gauche pour qu'il se superpose au logo 1
+        duration: duration,
+        ease: "power2.inOut",
+      });
+    }, delay * 1000); // Convertit le délai en millisecondes
   }, []);
 
   return (
     <header className="relative">
-      <div className="absolute"> {/* Ajoute un conteneur avec la classe absolute */}
+      <div className="absolute">
         <img
           src={logoSHADOW}
           alt="logo Shadow"
@@ -35,7 +38,7 @@ const Header = () => {
         <img
           src={logoSHADOWCOLOR}
           alt="logo Shadow Color"
-          className="w-32 absolute top-0 left-0" // Positionne le logo 2 en haut à gauche du conteneur
+          className="w-32 absolute top-0 left-0"
           id="logo2"
         />
       </div>
