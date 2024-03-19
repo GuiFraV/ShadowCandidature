1316f80f5a21939f32f926d9baf36197308232ad
GuiFraV <guillaume.franguiadakis@gmail.com>
2024-03-19 15:39:30 +0100

    Update Footer component and add GitHub logo

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,7 +1,11 @@
 import me from "../assets/images/me.png";
 import logoShadow from "../assets/images/logoSHADOW.svg";
-import logoShadowImage from '../assets/images/logoS.jpg'
+import logoShadowImage from '../assets/images/logoS.jpg';
+import githubLogo from '../assets/images/github-mark.svg';
+import githubLogoWhite from '../assets/images/github-mark-white.svg';
 
 export const photoMe = me;
 export const logoSHADOW = logoShadow;
 export const logoS = logoShadowImage;
+export const gitLogo = githubLogo;
+export const gitLogoWhite = githubLogoWhite;
