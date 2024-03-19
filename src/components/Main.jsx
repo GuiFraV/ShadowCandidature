6bd0a2d19c714622486368d867af8b579f162978
GuiFraV <guillaume.franguiadakis@gmail.com>
2024-03-19 18:52:55 +0100

    Add react-canvas-confetti library

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -36,18 +36,12 @@ const Main = () => {
         >
           Front End Developer, Franguiadakis Guillaume
         </h1>
-        <div className="box flex flex-col md:flex-row opacity-0">
-          <input
-            type="text"
-            id="email"
-            placeholder="Email Address"
-            className="w-full md:w-auto mb-2 md:mb-0 md:mr-2 px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-          />
-          <input
-            type="submit"
-            value="Add me"
-            className="w-full md:w-auto px-6 py-2 bg-[#923ADB] text-white font-bold rounded-md cursor-pointer hover:bg-[#7e32b8] transition duration-300"
-          />
+        <div className="box flex flex-col md:flex-row opacity-0 w-48">
+          <a
+            href='/src/assets/CV/CV-Guillaume_FRANGUIADAKIS.pdf'
+            download="CV-Guillaume_Franguiadakis.pdf"
+            className="flex justify-center w-full px-6 py-2 bg-[#3853c9] text-white font-bold rounded-md cursor-pointer hover:bg-[#4c63c9] transition duration-300"
+          >Mon CV</a>
         </div>
       </div>
 
